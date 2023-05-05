10baa5a22d0c2283b2329c6f1255ac4e433114ea
exosolarplanet <ecedenniz@gmail.com>
2023-05-05 23:04:43 +0100

    fix basic controller

--- src/main/java/com/revolut/Bornin/controller/BasicController.java
+++ src/main/java/com/revolut/Bornin/controller/BasicController.java
@@ -1,15 +1,15 @@
-package com.revolut.Bornin;
+package com.revolut.Bornin.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
 
 @RestController
 public class BasicController {
 
-    @GetMapping("health")
+    @GetMapping("/health")
     public ResponseEntity<String> getHealth(){
         return ResponseEntity.ok("healthy");
     }
-}
 
+}
