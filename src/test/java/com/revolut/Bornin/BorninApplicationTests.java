058ee01368004183f27a034fc7eb7f16dc45f499
exosolarplanet <ecedenniz@gmail.com>
2023-05-05 22:25:47 +0100

    add springboot project

---
+++ src/test/java/com/revolut/Bornin/BorninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revolut.Bornin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
