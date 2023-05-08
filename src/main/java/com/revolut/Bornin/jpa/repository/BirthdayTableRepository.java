91ecd03fec0b8cd1f2e25953ef385a4994f114fa
exosolarplanet <ecedenniz@gmail.com>
2023-05-08 22:30:14 +0100

    add cloudsql connection

--- src/main/java/com/revolut/Bornin/jpa/repository/BirthdayTableRepository.java
+++ src/main/java/com/revolut/Bornin/jpa/repository/BirthdayTableRepository.java
@@ -1,13 +1,7 @@
 package com.revolut.Bornin.jpa.repository;
 
 import com.revolut.Bornin.jpa.table.BirthdayTable;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDate;
 
 public interface BirthdayTableRepository extends JpaRepository<BirthdayTable, String> {
 
