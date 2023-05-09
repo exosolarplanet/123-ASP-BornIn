0112058afc87ed53d1c6bf253d49007f5f3f2ac5
exosolarplanet <ecedenniz@gmail.com>
2023-05-09 11:44:55 +0100

    comment out failing tests

--- src/test/java/com/revolut/Bornin/BorninApplicationTests.java
+++ src/test/java/com/revolut/Bornin/BorninApplicationTests.java
@@ -1,9 +1,15 @@
 package com.revolut.Bornin;
 
+import com.revolut.Bornin.Controller.BirthdayControllerTests;
 import com.revolut.Bornin.controller.BirthdayController;
+import com.revolut.Bornin.jpa.JpaTests;
+import com.revolut.Bornin.service.BirthdayServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
