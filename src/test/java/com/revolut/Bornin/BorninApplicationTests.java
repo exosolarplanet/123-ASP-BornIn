ae9bdf70231b74440791c2bb01cb949ae95ce2e2
exosolarplanet <ecedenniz@gmail.com>
2023-05-09 11:54:47 +0100

    fix syntax and add hydrate env step to build

--- src/test/java/com/revolut/Bornin/BorninApplicationTests.java
+++ src/test/java/com/revolut/Bornin/BorninApplicationTests.java
@@ -1,12 +1,9 @@
 package com.revolut.Bornin;
 
-import com.revolut.Bornin.Controller.BirthdayControllerTests;
+
 import com.revolut.Bornin.controller.BirthdayController;
-import com.revolut.Bornin.jpa.JpaTests;
-import com.revolut.Bornin.service.BirthdayServiceTest;
+
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
